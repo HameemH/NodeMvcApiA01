2a751228a07b0d0374924d1f011d16ab29329b7a
HameemH <hameemhossain2@gmail.com>
2022-09-04 11:03:57 +0600

    fs system

--- index.js
+++ index.js
@@ -2,10 +2,19 @@ const express = require('express');
 const app = express()
 const cors = require('cors');
 const port = process.env.PORT || 5000;
+const {readFile} = require('fs');
 app.use(cors());
 app.use(express.json());
+let data = []
+readFile("./data.json", 'utf8', (err, result) => {
+   if (err) {
+      console.log(err)
+      return
+   }
 
-
+  console.log(result);
+})
+console.log(data)
  app.get('/',(req,res)=>{
     res.send('Hello From Server')
  })
