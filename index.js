32e125184f4fa231ab84068b985de1d49f60c0e3
HameemH <hameemhossain2@gmail.com>
2022-09-12 01:47:40 +0600

    file system added with api integration

--- index.js
+++ index.js
@@ -2,22 +2,43 @@ const express = require('express');
 const app = express()
 const cors = require('cors');
 const port = process.env.PORT || 5000;
-const {readFile} = require('fs');
+const {readFile, writeFile} = require('fs');
 app.use(cors());
 app.use(express.json());
-let data = []
+let userdata = []
 readFile("./data.json", 'utf8', (err, result) => {
+   
    if (err) {
       console.log(err)
       return
    }
+   const data = JSON.parse(result)
+   
+   app.get('/',(req,res)=>{
+      const random = Math.floor(Math.random() * data.length);
+      res.send(data[random])
 
-  console.log(result);
+   })
+   app.post('/',async(req,res)=>{
+      console.log(data)
+      const newData = req.body;
+      const newArr = await data.push(newData)
+     
+      /* writeFile(
+         './data.json',
+         `${newArr}`,
+         (err, result) => {
+           if (err) {
+             console.log(err)
+             return
+           }
+           console.log('done with this task')
+         }
+       ) */
+        res.send('')
+   })
 })
-console.log(data)
- app.get('/',(req,res)=>{
-    res.send('Hello From Server')
- })
+ 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`)
   })
